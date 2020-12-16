3ee939842b7714154ad08af62cd502e283051e76
Sigifredo.Ramirez <sigi.16.ramiresco@gmail.com>
2020-12-16 14:34:24 -0600

    creacion de carpeta routes

--- server/server.js
+++ server/server.js
@@ -9,58 +9,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
  // parse application/json
 app.use(bodyParser.json());
 
- 
+
 app.get('/', function (req, res) {
-  res.send('<h1>bienvenido al servidor examen</h1>');
+    res.send('<h1>bienvenido al servidor examen</h1>');
 });
 
-app.get('/usuario', function (req, res) {
-    res.json({
-        ok: '200',
-        msg: 'usuario consultado'   
-    });
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
-    let nombre = req.body.nombre;
-    let body = req.body;
-
-    if(nombre == undefined){
-        res.status(400).json({
-            ok: 400,
-            msg: 'enviar el nombre'
-        });
-
-    }else{
-
-        res.json({
-            ok: '200',
-            msg: 'usuario insertado',
-            body: body
-        });
-    }
-});
-
-app.put('/usuario/:id/:nombre', function (req, res) {
-    let id = req.params.id;
-    let nombre = req.params.nombre;
-
-    res.json({
-        ok: '200',
-        msg: 'usuario actualizado',
-        id: id,
-        nombre, nombre
-    });
-});
-
-app.delete('/usuario/:id', function (req, res) {
-    let id = req.params.id
-    res.json({
-        ok: '200',
-        msg: 'usuario eliminado',
-        id: id
-    });
-});
  
 mongoose.connect('mongodb://localhost:27017/empresa', {
   useNewUrlParser: true,
